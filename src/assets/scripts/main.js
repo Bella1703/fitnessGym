37c301635f057dd4e3287cd8ebbe9a8be01a6fc1
Bella1703 <bella.vinogradova@gmail.com>
2021-05-20 22:54:52 +0300

    started main page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,7 +1,5 @@
-
 let burgerMenu = document.querySelector('.burgerMenu');
 function menuChange() {
-
   if(burgerMenu.classList.contains("change")) {
     document.body.querySelector('.nav-list').classList.remove('nav-list__move-down');
     document.body.querySelector('.nav-list').classList.add('nav-list__move-up');
@@ -10,7 +8,6 @@ function menuChange() {
     document.body.querySelector('.nav-list').classList.remove('nav-list__move-up');
     document.body.querySelector('.nav-list').classList.add('nav-list__move-down');
   }
-
   burgerMenu.classList.toggle("change");
 }
 burgerMenu.addEventListener('click', menuChange);
